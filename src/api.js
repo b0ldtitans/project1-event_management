ec1315b30e7aaed8fb941abcf152012f50a78d32
unknown <thareqazani@gmail.com>
2023-09-16 17:31:37 +0700

    fix api

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL:
-    "https://my-json-server.typicode.com/b0ldtitans/mini_project_event-management",
+    "https://my-json-server.typicode.com/b0ldtitans/project1-event_management/",
 });
 
 export default instance;
