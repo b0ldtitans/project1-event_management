f44279b1d8736c6e6d41a5957268d2c23bdd84d9
unknown <thareqazani@gmail.com>
2023-09-16 16:29:21 +0700

    minor bug fix

--- src/components/registerModal.js
+++ src/components/registerModal.js
@@ -105,7 +105,7 @@ export default function Register() {
                 type="text"
                 name="name"
                 id="name"
-                placeholder="Christina"
+                placeholder="Your Name"
                 class="bg-orange-50 border border-orange-300 text-orange-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-orange-700 dark:border-orange-600 dark:placeholder-orange-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 required="true"
               />
@@ -142,7 +142,6 @@ export default function Register() {
                 id="referral_code"
                 class="bg-orange-50 border border-orange-300 text-orange-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-orange-700 dark:border-orange-600 dark:placeholder-orange-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="12345"
-                required=""
               />
             </div>
 
